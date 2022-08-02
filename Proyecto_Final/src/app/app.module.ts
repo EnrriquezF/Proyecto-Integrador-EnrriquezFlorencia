dd1aab700f667a52bb01400ee0d3dbd9054630e5
EnrriquezF <enrriquezflorencia97@gmail.com>
2022-08-02 19:27:26 -0300

    Detalles de header

--- Proyecto_Final/src/app/app.module.ts
+++ Proyecto_Final/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoArgProgComponent } from './component/logo-arg-prog/logo-arg-prog.component';
+import { RRSSComponent } from './component/rrss/rrss.component';
+import { BannerComponent } from './component/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RRSSComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
