9a8e481065f6f427facf50b993d792780eb3b768
EnrriquezF <enrriquezflorencia97@gmail.com>
2022-08-06 04:00:34 -0300

    initializr

---
+++ Proyecto_Final/BackEnd/src/main/java/com/portfolioenfr/portfolioenrfr/PortfolioenrfrApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioenfr.portfolioenrfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioenrfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioenrfrApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Final/BackEnd/src/test/java/com/portfolioenfr/portfolioenrfr/PortfolioenrfrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioenfr.portfolioenrfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioenrfrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
